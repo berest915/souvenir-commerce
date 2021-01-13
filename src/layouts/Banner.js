ceb4e0988cef6a7b8ee9a46301f51b82d90ed40c
berest915 <berest915@gmail.com>
2021-01-13 23:40:00 +0800

    first commit > add top banner

---
+++ src/layouts/Banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Wrapper } from "./BannerStyle";
+
+const Banner = () => {
+  return (
+    <Wrapper>
+      <p className="strong">Get Free Shipping! Use Code “FREESHIP121b6f00e”</p>
+      <p className="small">
+        Order minimum $45 US/$80 INTL. Ends Jan 31 '21 5PM CST. Excludes INTL
+        Wall Art and Shoes.
+      </p>
+    </Wrapper>
+  );
+};
+
+export default Banner;
---
+++ src/layouts/BannerStyle.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  padding: 0.7rem 1rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  color: white;
+  
+  
+  .strong {
+    font-size: 1.4rem;
+    padding-bottom: 5px;
+  }
+  .small{
+    font-size: .9rem;
+  }
+`
